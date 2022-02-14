a8648a07ce7699bdf644fc36a367d23e37751bba
Tusar <tusarahmed141@gmail.com>
2022-02-15 02:03:15 +0600

    increased and decreased just

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
-// Declared and initialized two buttons
-const increaseBtn = document.getElementById('increase-btn');
-const decreasedBtn = document.getElementById('decreased-btn');
+// Initialize this variable for iphone
+const iphonePlus = document.getElementById('iphone-plus');
+const iphoneMinus = document.getElementById('iphone-minus');
+
+// Initialize this variable for casing
+const casePlus = document.getElementById('case-plus');
+const caseMinus = document.getElementById('case-minus');
+
+
 
 // Increased and decreased case function
-const caseUpdate = isIncreased => {
-    const caseInput = document.getElementById('case-input');
+const amountUpdate = (userInput, userOutput, btn, multiply, isIncreased) => {
+    const caseInput = document.getElementById(userInput);
     let caseInputText = caseInput.value;
     const caseInputValue = parseFloat(caseInputText);
     
-    if (isIncreased) {             
-        // Case increased
+    if (isIncreased) {         
         caseInputText = caseInputValue + 1;
-        // Remove attribute
-        decreasedBtn.removeAttribute('disabled')
+        btn.removeAttribute('disabled')
     } else {
         if (caseInputValue > 0) {
             caseInputText = caseInputValue - 1;
         } else {
-            decreasedBtn.setAttribute('disabled', true)
+            btn.setAttribute('disabled', true)
         }              
     }
     caseInput.value = caseInputText;
 
     // Get case total
-    const caseTotal = document.getElementById('case-total')
-    // Amount increased
-    caseTotal.innerText = caseInputText * 59;
+    const caseTotal = document.getElementById(userOutput)
+    caseTotal.innerText = caseInputText * multiply;
+
+    // Total Money
+    // const subTotal = document.getElementById('sub-total');
+    // let subTotalAmount = subTotal.innerText;
+    // subTotalAmount = parseFloat(subTotalAmount) + parseFloat(caseTotal.innerText);
+    // subTotal.innerText = subTotalAmount;
+
+    // const tax = document.getElementById('tax');
+    // let taxAmount = subTotal.innerText;
+
+    // const total = document.getElementById('total');
+    // let totalMoney = subTotal.innerText;
+     
 }
  
-// increased functionalities
-increaseBtn.addEventListener('click', e => {
-    caseUpdate(true);
+// increased functionalities for iphone 
+iphonePlus.addEventListener('click', e => {
+    amountUpdate('iphone-input', 'iphone-total', iphoneMinus, 1219, true);
 })
 
-// Decreased functionalities
-decreasedBtn.addEventListener('click', e => {
-    caseUpdate(false);
+// Decreased functionalities for iphone
+iphoneMinus.addEventListener('click', e => {
+    amountUpdate('iphone-input', 'iphone-total', iphoneMinus, 1219, false);
+})
+
+// increased functionalities for casing 
+casePlus.addEventListener('click', e => {
+    amountUpdate('case-input', 'case-total', caseMinus, 59, true);
+})
+
+// Decreased functionalities for casing
+caseMinus.addEventListener('click', e => {
+    amountUpdate('case-input', 'case-total', caseMinus, 59, false);
+
 })
